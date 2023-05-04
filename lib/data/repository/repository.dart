e1298095f2d0ecfd774f4d4e2021b744f67d1295
farhan.riaz <farhan.riaz@mrs-technologies.com>
2023-05-05 01:32:13 +0500

    -added repository implementation for user authentication methods.

---
+++ lib/data/repository/repository.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+import 'package:second_opinion_app/data/network/apis/user/authentication_api.dart';
+import 'package:second_opinion_app/data/sharedpref/shared_preference_helper.dart';
+import 'package:second_opinion_app/models/authentication/login_request.dart';
+import 'package:second_opinion_app/models/authentication/login_user_response.dart';
+import 'package:second_opinion_app/models/authentication/register_user_response.dart';
+import 'package:second_opinion_app/models/authentication/registration_request.dart';
+
+class Repository {
+  // api objects
+  final AuthenticationApi _authenticationApi;
+
+  // shared pref object
+  final SharedPreferenceHelper _sharedPrefsHelper;
+
+  // constructor
+  Repository(this._authenticationApi, this._sharedPrefsHelper);
+
+  // Register:------------------------------------------------------------------
+
+  Future<RegisterUserResponse> register(
+      String name, String password, String email) async {
+    return await _authenticationApi
+        .registerUser(RegistrationRequest(
+      name: name,
+      email: email,
+      password: password,
+    ))
+        .then((response) {
+      if (response != null) {
+        print("User registered");
+        return response;
+      } else {
+        print("User already registered");
+        return RegisterUserResponse();
+      }
+    });
+  }
+
+  // Login:---------------------------------------------------------------------
+  Future<LoginUserResponse> login(String email, String password) async {
+    return await _authenticationApi
+        .loginUser(LoginRequest(email: email, password: password))
+        .then((response) {
+      if (response.token != null) {
+        saveIsLoggedIn(true);
+        saveAuthenticationToken(response.token!);
+        return response;
+      } else {
+        return response;
+      }
+    });
+  }
+
+  Future<void> saveIsLoggedIn(bool value) =>
+      _sharedPrefsHelper.saveIsLoggedIn(value);
+
+  Future<void> saveAuthenticationToken(String value) =>
+      _sharedPrefsHelper.saveAuthToken(value);
+
+  Future<bool> get isLoggedIn => _sharedPrefsHelper.isLoggedIn;
+
+  Future<String?> get authToken async => await _sharedPrefsHelper.authToken;
+
+  // Theme: --------------------------------------------------------------------
+  Future<void> changeBrightnessToDark(bool value) =>
+      _sharedPrefsHelper.changeBrightnessToDark(value);
+
+  bool get isDarkMode => _sharedPrefsHelper.isDarkMode;
+
+  // Language: -----------------------------------------------------------------
+  Future<void> changeLanguage(String value) =>
+      _sharedPrefsHelper.changeLanguage(value);
+
+  String? get currentLanguage => _sharedPrefsHelper.currentLanguage;
+}
