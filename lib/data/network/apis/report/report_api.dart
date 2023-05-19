1c4fad95e5222f3aea815aefb0fb093a9524c278
farhaanwirk <farhan.riaz@mrs-electronic.com>
2023-05-20 03:32:50 +0500

    feat:added response model class for report.

--- lib/data/network/apis/report/report_api.dart
+++ lib/data/network/apis/report/report_api.dart
@@ -1,3 +1,7 @@
+import 'package:dio/dio.dart';
+import 'package:second_opinion_app/models/report/upload_report_response.dart';
+
+import '../../constants/endpoints.dart';
 import '../../dio_client.dart';
 import '../../rest_client.dart';
 
@@ -10,5 +14,32 @@ class ReportApi {
 
   // injecting dio instance
   ReportApi(this._dioClient, this._restClient);
+
+
+
+  Future<UploadReportResponse> uploadDocument(RegistrationRequest registerRequest) async {
+    try {
+      FormData formData = FormData.fromMap(registerRequest.toJson());
+
+      final res = await _dioClient.post(
+        Endpoints.uploadDocument,
+        data: formData,
+        options: Options(
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        ),
+      );
+
+      if (res != null) {
+        return UploadReportResponse.fromJson(res);
+      } else {
+        throw Exception("Null response received!");
+      }
+    } catch (e) {
+      print(e.toString());
+      throw e;
+    }
+  }
 
 }
