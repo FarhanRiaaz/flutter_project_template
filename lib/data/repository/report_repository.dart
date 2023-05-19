1a78af7e592c075ae48995c9f300c4ca73c996d3
farhaanwirk <farhan.riaz@mrs-electronic.com>
2023-05-20 03:55:09 +0500

    feat:added store method to upload document.

--- lib/data/repository/report_repository.dart
+++ lib/data/repository/report_repository.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:second_opinion_app/models/report/upload_report_response.dart';
+
 import '../network/apis/report/report_api.dart';
 import '../sharedpref/shared_preference_helper.dart';
 
@@ -5,10 +9,23 @@ class ReportRepository {
   // api objects
   final ReportApi _reportApi;
 
-
   // shared pref object
   final SharedPreferenceHelper _sharedPrefsHelper;
 
   // constructor
   ReportRepository(this._reportApi, this._sharedPrefsHelper);
+
+  Future<UploadReportResponse> uploadDocument(
+    String fileName,
+    String fileType,
+    File documentFile,
+    int userId,
+  ) async {
+    final authToken = await _sharedPrefsHelper.authToken;
+    print("uploadReport$authToken");
+    return await _reportApi.uploadDocument(
+        fileName, fileType, documentFile, userId, authToken!);
+  }
+
+  Future<String?> get authToken async => await _sharedPrefsHelper.authToken;
 }
--- lib/stores/report/report_store.dart
+++ lib/stores/report/report_store.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:mobx/mobx.dart';
 import 'package:second_opinion_app/data/repository/report_repository.dart';
+import 'package:second_opinion_app/models/report/upload_report_response.dart';
 
 part 'report_store.g.dart';
 
@@ -8,7 +11,49 @@ class ReportStore = _ReportStore with _$ReportStore;
 abstract class _ReportStore with Store {
   final ReportRepository _reportRepository;
 
+  // empty responses:-----------------------------------------------------------
+  static ObservableFuture<UploadReportResponse> emptyUploadReportResponse =
+      ObservableFuture.value(UploadReportResponse());
+
+  @observable
+  ObservableFuture<UploadReportResponse> reportFuture =
+      emptyUploadReportResponse;
+
+  @observable
+  UploadReportResponse? currentReportResponse;
+
+  @observable
+  String? fileName;
+  @observable
+  String? fileType;
+  @observable
+  File? documentFile;
+  @observable
+  int? userId;
+
+  @computed
+  bool get isUploadInProcess => reportFuture.status == FutureStatus.pending;
+
+  @action
+  Future uploadReport() async {
+    final future = _reportRepository.uploadDocument(
+        fileName!, fileType!, documentFile!, userId!);
+    reportFuture = ObservableFuture(future);
+    await future.then((value) async {
+      if (value.id != null) {
+        currentReportResponse = value;
+      } else {
+        print('failed to uploadReport\nSomething went wrong');
+      }
+    }).catchError((e) {
+      print(e);
+
+      print('failed to uploadReport\nSomething went wrong!\n${e.toString()}');
+      throw e;
+    });
+  }
+
   _ReportStore(
-      ReportRepository reportRepository,
-      ) : this._reportRepository = reportRepository {}
+    ReportRepository reportRepository,
+  ) : this._reportRepository = reportRepository {}
 }
