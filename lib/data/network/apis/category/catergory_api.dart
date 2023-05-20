b43d872820a32fbeab9125543fb869ee16924d41
farhaanwirk <farhan.riaz@mrs-electronic.com>
2023-05-20 05:46:23 +0500

    feat:added api and repo to get all categories and form for those categories.

--- lib/data/network/apis/category/catergory_api.dart
+++ lib/data/network/apis/category/catergory_api.dart
@@ -5,6 +5,8 @@ import 'package:second_opinion_app/models/authentication/login_request.dart';
 import 'package:second_opinion_app/models/authentication/login_user_response.dart';
 import 'package:second_opinion_app/models/authentication/register_user_response.dart';
 import 'package:second_opinion_app/models/authentication/registration_request.dart';
+import 'package:second_opinion_app/models/categories/all_category_response.dart';
+import 'package:second_opinion_app/models/categories/category_instance_response.dart';
 
 import '../../constants/endpoints.dart';
 
@@ -12,15 +14,13 @@ class CategoryApi {
   // dio instance
   final DioClient _dioClient;
 
-
   // injecting dio instance
   CategoryApi(this._dioClient);
 
   /// Method to get list of categories
   ///
-  Future<RegisterUserResponse> getALlCategories(String authToken) async {
+  Future<GetAllCategoryResponse> getALlCategories(String authToken) async {
     try {
-
       final res = await _dioClient.get(
         Endpoints.getCategories,
         options: Options(
@@ -32,7 +32,7 @@ class CategoryApi {
       );
 
       if (res != null) {
-        return RegisterUserResponse.fromJson(res);
+        return GetAllCategoryResponse.fromJson(res);
       } else {
         throw Exception("Null response received!");
       }
@@ -42,4 +42,28 @@ class CategoryApi {
     }
   }
 
+//todo shahzaib we need to test and add things to following method
+  Future<CategoryInstanceResponse> getFormByCategory(
+      String authToken, int catType) async {
+    try {
+      final res = await _dioClient.get(
+        Endpoints.getFormByCatId,
+        options: Options(
+          headers: {
+            'Authorization': 'Token $authToken',
+            'Content-Type': 'multipart/form-data',
+          },
+        ),
+      );
+
+      if (res != null) {
+        return CategoryInstanceResponse.fromJson(res);
+      } else {
+        throw Exception("Null response received!");
+      }
+    } catch (e) {
+      print(e.toString());
+      throw e;
+    }
+  }
 }
---
+++ lib/data/repository/category_repository.dart
@@ -0,0 +1,30 @@
+import 'package:second_opinion_app/data/network/apis/category/catergory_api.dart';
+import 'package:second_opinion_app/models/categories/all_category_response.dart';
+import 'package:second_opinion_app/models/categories/category_instance_response.dart';
+
+import '../sharedpref/shared_preference_helper.dart';
+
+class CategoryRepository {
+  // api objects
+  final CategoryApi _categoryApi;
+
+  // shared pref object
+  final SharedPreferenceHelper _sharedPrefsHelper;
+
+  // constructor
+  CategoryRepository(this._categoryApi, this._sharedPrefsHelper);
+
+  Future<GetAllCategoryResponse> getAllCategories() async {
+    final authToken = await _sharedPrefsHelper.authToken;
+    print("getAllCategories$authToken");
+    return await _categoryApi.getALlCategories(authToken!);
+  }
+
+  Future<CategoryInstanceResponse> getFormByCategory(int catType) async {
+    final authToken = await _sharedPrefsHelper.authToken;
+    print("getFormByCategory$authToken");
+    return await _categoryApi.getFormByCategory(authToken!, catType);
+  }
+
+  Future<String?> get authToken async => await _sharedPrefsHelper.authToken;
+}
