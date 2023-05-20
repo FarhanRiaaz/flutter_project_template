5ff6a88e0d11557ad28112aaccbac1fd8a755284
farhaanwirk <farhan.riaz@mrs-electronic.com>
2023-05-20 06:04:11 +0500

    fix:added injection for category flow.

--- lib/di/components/service_locator.dart
+++ lib/di/components/service_locator.dart
@@ -1,14 +1,16 @@
-
 import 'package:second_opinion_app/data/local/datasources/post/post_datasource.dart';
+import 'package:second_opinion_app/data/network/apis/category/catergory_api.dart';
 import 'package:second_opinion_app/data/network/apis/posts/post_api.dart';
 import 'package:second_opinion_app/data/network/apis/profile/profile_api.dart';
 import 'package:second_opinion_app/data/network/apis/report/report_api.dart';
 import 'package:second_opinion_app/data/network/dio_client.dart';
 import 'package:second_opinion_app/data/network/rest_client.dart';
+import 'package:second_opinion_app/data/repository/category_repository.dart';
 import 'package:second_opinion_app/data/repository/repository.dart';
 import 'package:second_opinion_app/data/sharedpref/shared_preference_helper.dart';
 import 'package:second_opinion_app/di/module/local_module.dart';
 import 'package:second_opinion_app/di/module/network_module.dart';
+import 'package:second_opinion_app/stores/category/category_store.dart';
 import 'package:second_opinion_app/stores/error/error_store.dart';
 import 'package:second_opinion_app/stores/form/form_store.dart';
 import 'package:second_opinion_app/stores/language/language_store.dart';
@@ -28,41 +30,46 @@ import '../../stores/report/report_store.dart';
 final getIt = GetIt.instance;
 
 Future<void> setupLocator() async {
-
   // factories:-----------------------------------------------------------------
   getIt.registerFactory(() => ErrorStore());
   getIt.registerFactory(() => FormStore());
 
-
   // async singletons:----------------------------------------------------------
   getIt.registerSingletonAsync<Database>(() => LocalModule.provideDatabase());
-  getIt.registerSingletonAsync<SharedPreferences>(() => LocalModule.provideSharedPreferences());
+  getIt.registerSingletonAsync<SharedPreferences>(
+      () => LocalModule.provideSharedPreferences());
 
   // singletons:----------------------------------------------------------------
-  getIt.registerSingleton(SharedPreferenceHelper(await getIt.getAsync<SharedPreferences>()));
-  getIt.registerSingleton<Dio>(NetworkModule.provideDio(getIt<SharedPreferenceHelper>()));
+  getIt.registerSingleton(
+      SharedPreferenceHelper(await getIt.getAsync<SharedPreferences>()));
+  getIt.registerSingleton<Dio>(
+      NetworkModule.provideDio(getIt<SharedPreferenceHelper>()));
   getIt.registerSingleton(DioClient(getIt<Dio>()));
   getIt.registerSingleton(RestClient());
 
   // api's:---------------------------------------------------------------------
   getIt.registerSingleton(PostApi(getIt<DioClient>(), getIt<RestClient>()));
-  getIt.registerSingleton(AuthenticationApi(getIt<DioClient>(), getIt<RestClient>()));
+  getIt.registerSingleton(
+      AuthenticationApi(getIt<DioClient>(), getIt<RestClient>()));
   getIt.registerSingleton(ProfileApi(getIt<DioClient>()));
   getIt.registerSingleton(ReportApi(getIt<DioClient>()));
+  getIt.registerSingleton(CategoryApi(getIt<DioClient>()));
 
   // data sources
   getIt.registerSingleton(PostDataSource(await getIt.getAsync<Database>()));
 
   // repository:----------------------------------------------------------------
-  getIt.registerSingleton(Repository(
-    getIt<AuthenticationApi>(),
-    getIt<SharedPreferenceHelper>(),
-    getIt<ProfileApi>()
-  ));
+  getIt.registerSingleton(Repository(getIt<AuthenticationApi>(),
+      getIt<SharedPreferenceHelper>(), getIt<ProfileApi>()));
 
   getIt.registerSingleton(ReportRepository(
-      getIt<ReportApi>(),
-      getIt<SharedPreferenceHelper>(),
+    getIt<ReportApi>(),
+    getIt<SharedPreferenceHelper>(),
+  ));
+
+  getIt.registerSingleton(CategoryRepository(
+    getIt<CategoryApi>(),
+    getIt<SharedPreferenceHelper>(),
   ));
 
   // stores:--------------------------------------------------------------------
@@ -72,4 +79,5 @@ Future<void> setupLocator() async {
   getIt.registerSingleton(UserStore(getIt<Repository>()));
   getIt.registerSingleton(ProfileStore(getIt<Repository>()));
   getIt.registerSingleton(ReportStore(getIt<ReportRepository>()));
+  getIt.registerSingleton(CategoryStore(getIt<CategoryRepository>()));
 }
---
+++ lib/stores/category/category_store.g.dart
@@ -0,0 +1,122 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'category_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$CategoryStore on _CategoryStore, Store {
+  Computed<bool>? _$isAllCategoriesInProcessComputed;
+
+  @override
+  bool get isAllCategoriesInProcess => (_$isAllCategoriesInProcessComputed ??=
+          Computed<bool>(() => super.isAllCategoriesInProcess,
+              name: '_CategoryStore.isAllCategoriesInProcess'))
+      .value;
+  Computed<bool>? _$isCategoriesInstanceInProcessComputed;
+
+  @override
+  bool get isCategoriesInstanceInProcess =>
+      (_$isCategoriesInstanceInProcessComputed ??= Computed<bool>(
+              () => super.isCategoriesInstanceInProcess,
+              name: '_CategoryStore.isCategoriesInstanceInProcess'))
+          .value;
+
+  late final _$allCategoryFutureAtom =
+      Atom(name: '_CategoryStore.allCategoryFuture', context: context);
+
+  @override
+  ObservableFuture<AllCategoryList> get allCategoryFuture {
+    _$allCategoryFutureAtom.reportRead();
+    return super.allCategoryFuture;
+  }
+
+  @override
+  set allCategoryFuture(ObservableFuture<AllCategoryList> value) {
+    _$allCategoryFutureAtom.reportWrite(value, super.allCategoryFuture, () {
+      super.allCategoryFuture = value;
+    });
+  }
+
+  late final _$allCategoryInstanceFutureAtom =
+      Atom(name: '_CategoryStore.allCategoryInstanceFuture', context: context);
+
+  @override
+  ObservableFuture<CategoryInstanceResponse> get allCategoryInstanceFuture {
+    _$allCategoryInstanceFutureAtom.reportRead();
+    return super.allCategoryInstanceFuture;
+  }
+
+  @override
+  set allCategoryInstanceFuture(
+      ObservableFuture<CategoryInstanceResponse> value) {
+    _$allCategoryInstanceFutureAtom
+        .reportWrite(value, super.allCategoryInstanceFuture, () {
+      super.allCategoryInstanceFuture = value;
+    });
+  }
+
+  late final _$allCategoryListAtom =
+      Atom(name: '_CategoryStore.allCategoryList', context: context);
+
+  @override
+  AllCategoryList? get allCategoryList {
+    _$allCategoryListAtom.reportRead();
+    return super.allCategoryList;
+  }
+
+  @override
+  set allCategoryList(AllCategoryList? value) {
+    _$allCategoryListAtom.reportWrite(value, super.allCategoryList, () {
+      super.allCategoryList = value;
+    });
+  }
+
+  late final _$categoryInstanceResponseAtom =
+      Atom(name: '_CategoryStore.categoryInstanceResponse', context: context);
+
+  @override
+  CategoryInstanceResponse? get categoryInstanceResponse {
+    _$categoryInstanceResponseAtom.reportRead();
+    return super.categoryInstanceResponse;
+  }
+
+  @override
+  set categoryInstanceResponse(CategoryInstanceResponse? value) {
+    _$categoryInstanceResponseAtom
+        .reportWrite(value, super.categoryInstanceResponse, () {
+      super.categoryInstanceResponse = value;
+    });
+  }
+
+  late final _$getAllCategoriesAsyncAction =
+      AsyncAction('_CategoryStore.getAllCategories', context: context);
+
+  @override
+  Future<dynamic> getAllCategories() {
+    return _$getAllCategoriesAsyncAction.run(() => super.getAllCategories());
+  }
+
+  late final _$getFormByCategoryAsyncAction =
+      AsyncAction('_CategoryStore.getFormByCategory', context: context);
+
+  @override
+  Future<dynamic> getFormByCategory() {
+    return _$getFormByCategoryAsyncAction.run(() => super.getFormByCategory());
+  }
+
+  @override
+  String toString() {
+    return '''
+allCategoryFuture: ${allCategoryFuture},
+allCategoryInstanceFuture: ${allCategoryInstanceFuture},
+allCategoryList: ${allCategoryList},
+categoryInstanceResponse: ${categoryInstanceResponse},
+isAllCategoriesInProcess: ${isAllCategoriesInProcess},
+isCategoriesInstanceInProcess: ${isCategoriesInstanceInProcess}
+    ''';
+  }
+}
